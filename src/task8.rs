1139f515a77fde7f36234109a6fb25a67a84509b
Max Bukhalov <buchalovmaxim@gmail.com>
2025-12-08 17:37:56 +0300

    finished task8

--- src/task8.rs
+++ src/task8.rs
@@ -1,45 +1,51 @@
 use std::fs::read_to_string;
-use itertools::Itertools;
+use itertools::{Itertools, iproduct};
+use std::collections::HashMap;
+
+
+fn distance(a: (u64, u64, u64), b: (u64, u64, u64)) -> u64 {
+    (a.0.max(b.0) - a.0.min(b.0)) * (a.0.max(b.0) - a.0.min(b.0)) +
+    (a.1.max(b.1) - a.1.min(b.1)) * (a.1.max(b.1) - a.1.min(b.1)) +
+    (a.2.max(b.2) - a.2.min(b.2)) * (a.2.max(b.2) - a.2.min(b.2))
+}
 
 
 fn part1(junction_boxes: Vec<(u64, u64, u64)>, connections_limit: u64) -> u64 {
     let mut circuits: Vec<u64> = Vec::new();
     circuits.resize(junction_boxes.len(), 1);
 
-    let mut junction_box_curcuit_idx: Vec<usize> = (0 .. circuits.len()).map(|x| x).collect();
-
-    for _c in 0 .. connections_limit-1 {
-        let mut nearest_junction_box_indices = (usize::max_value(), usize::max_value());
-        let mut distance_to_nearest_junction_box = u64::max_value();
-        for i in 0 .. junction_boxes.len() {
-            for j in 0 .. junction_boxes.len() {
-                if i != j && junction_box_curcuit_idx[i] != junction_box_curcuit_idx[j] {
-                    let distance = (junction_boxes[i].0.max(junction_boxes[j].0) - junction_boxes[i].0.min(junction_boxes[j].0)) * (junction_boxes[i].0.max(junction_boxes[j].0) - junction_boxes[i].0.min(junction_boxes[j].0)) + 
-                                    (junction_boxes[i].1.max(junction_boxes[j].1) - junction_boxes[i].1.min(junction_boxes[j].1)) * (junction_boxes[i].1.max(junction_boxes[j].1) - junction_boxes[i].1.min(junction_boxes[j].1)) + 
-                                    (junction_boxes[i].2.max(junction_boxes[j].2) - junction_boxes[i].2.min(junction_boxes[j].2)) * (junction_boxes[i].2.max(junction_boxes[j].2) - junction_boxes[i].2.min(junction_boxes[j].2));
-                    if distance < distance_to_nearest_junction_box {
-                        nearest_junction_box_indices = (i, j);
-                        distance_to_nearest_junction_box = distance;
-                    }
-                }
-            }
-        }
-        circuits[junction_box_curcuit_idx[nearest_junction_box_indices.0]] += circuits[junction_box_curcuit_idx[nearest_junction_box_indices.1]];
-        circuits[junction_box_curcuit_idx[nearest_junction_box_indices.1]] = 0;
-        for j in 0 .. junction_box_curcuit_idx.len() {
-            if junction_box_curcuit_idx[j] == junction_box_curcuit_idx[nearest_junction_box_indices.1] {
-                junction_box_curcuit_idx[j] = junction_box_curcuit_idx[nearest_junction_box_indices.0];
-            }
-        }
+    let mut circuits_ids: HashMap<(u64, u64, u64), usize> = HashMap::new();
+    for (idx, junction_box) in junction_boxes.iter().enumerate() {
+        circuits_ids.insert(*junction_box, idx);
     }
 
-    circuits.iter().unique().fold(1, |acc, x| {
-        if *x == 0 {
-            acc
-        } else {
-            acc * *x
+    let connections: Vec<((u64, u64, u64), (u64, u64, u64))> = iproduct!(junction_boxes.clone(), junction_boxes.clone()).sorted_by(|a, b| {
+        let distance_a = distance(a.0, a.1);
+        let distance_b = distance(b.0, b.1);
+        distance_a.cmp(&distance_b)
+    }).filter(|x| distance(x.0, x.1) > 0).step_by(2).collect();
+
+    let mut connections_made = 0;
+    for (connection_a, connection_b) in connections {
+        if connections_made == connections_limit {
+            break;
         }
-    })
+        let circuit_id_a = *circuits_ids.get(&connection_a).unwrap();
+        let circuit_id_b = *circuits_ids.get(&connection_b).unwrap();
+        if circuit_id_a == circuit_id_b {
+            connections_made += 1;
+            continue;
+        }
+        circuits[circuit_id_a] += circuits[circuit_id_b];
+        circuits[circuit_id_b] = 0;
+        for (_, circuit_id) in circuits_ids.iter_mut() {
+            if *circuit_id == circuit_id_b {
+                *circuit_id = circuit_id_a;
+            }
+        }
+        connections_made += 1;
+    }
+    circuits.iter().unique().filter(|&x| *x != 0).sorted_by(|a, b| b.cmp(a)).take(3).fold(1, |acc, x| acc * x)
 }
 
 
@@ -50,51 +56,44 @@ pub fn execute_part1_from_input(input: &str) {
 }
 
 
-fn part2(junction_boxes: Vec<(u64, u64, u64)>, connections_limit: u64) -> u64 {
-    let mut circuits: Vec<u64> = Vec::new();
-    circuits.resize(junction_boxes.len(), 1);
-
-    let mut junction_box_curcuit_idx: Vec<usize> = (0 .. circuits.len()).map(|x| x).collect();
-
-    for _c in 0 .. connections_limit-1 {
-        let mut nearest_junction_box_indices = (usize::max_value(), usize::max_value());
-        let mut distance_to_nearest_junction_box = u64::max_value();
-        for i in 0 .. junction_boxes.len() {
-            for j in 0 .. junction_boxes.len() {
-                if i != j && junction_box_curcuit_idx[i] != junction_box_curcuit_idx[j] {
-                    let distance = (junction_boxes[i].0.max(junction_boxes[j].0) - junction_boxes[i].0.min(junction_boxes[j].0)) * (junction_boxes[i].0.max(junction_boxes[j].0) - junction_boxes[i].0.min(junction_boxes[j].0)) + 
-                                    (junction_boxes[i].1.max(junction_boxes[j].1) - junction_boxes[i].1.min(junction_boxes[j].1)) * (junction_boxes[i].1.max(junction_boxes[j].1) - junction_boxes[i].1.min(junction_boxes[j].1)) + 
-                                    (junction_boxes[i].2.max(junction_boxes[j].2) - junction_boxes[i].2.min(junction_boxes[j].2)) * (junction_boxes[i].2.max(junction_boxes[j].2) - junction_boxes[i].2.min(junction_boxes[j].2));
-                    if distance < distance_to_nearest_junction_box {
-                        nearest_junction_box_indices = (i, j);
-                        distance_to_nearest_junction_box = distance;
-                    }
-                }
-            }
-        }
-        circuits[junction_box_curcuit_idx[nearest_junction_box_indices.0]] += circuits[junction_box_curcuit_idx[nearest_junction_box_indices.1]];
-        circuits[junction_box_curcuit_idx[nearest_junction_box_indices.1]] = 0;
-        for j in 0 .. junction_box_curcuit_idx.len() {
-            if junction_box_curcuit_idx[j] == junction_box_curcuit_idx[nearest_junction_box_indices.1] {
-                junction_box_curcuit_idx[j] = junction_box_curcuit_idx[nearest_junction_box_indices.0];
-            }
-        }
+fn part2(junction_boxes: Vec<(u64, u64, u64)>) -> u64 {
+    let mut circuits_ids: HashMap<(u64, u64, u64), usize> = HashMap::new();
+    for (idx, junction_box) in junction_boxes.iter().enumerate() {
+        circuits_ids.insert(*junction_box, idx);
     }
 
-    circuits.iter().unique().fold(1, |acc, x| {
-        if *x == 0 {
-            acc
-        } else {
-            acc * *x
+    let connections: Vec<((u64, u64, u64), (u64, u64, u64))> = iproduct!(junction_boxes.clone(), junction_boxes.clone()).sorted_by(|a, b| {
+        let distance_a = distance(a.0, a.1);
+        let distance_b = distance(b.0, b.1);
+        distance_a.cmp(&distance_b)
+    }).filter(|x| distance(x.0, x.1) > 0).step_by(2).collect();
+
+    let mut active_circuits = junction_boxes.len();
+    let mut result: u64 = 0;
+    while active_circuits > 1 {
+        for (connection_a, connection_b) in &connections {
+            let circuit_id_a = *circuits_ids.get(&connection_a).unwrap();
+            let circuit_id_b = *circuits_ids.get(&connection_b).unwrap();
+            if circuit_id_a == circuit_id_b {
+                continue;
+            }
+            for (_, circuit_id) in circuits_ids.iter_mut() {
+                if *circuit_id == circuit_id_b {
+                    *circuit_id = circuit_id_a;
+                }
+            }
+            result = connection_a.0 * connection_b.0;
+            active_circuits -= 1;
         }
-    })
+    }
+    result
 }
 
 
 pub fn execute_part2_from_input(input: &str) {
     let file_input = read_to_string(input).expect("Couldn't read file");
     let junction_boxes: Vec<(u64, u64, u64)> = file_input.lines().map(|x| x.split(',').map(|n| n.parse::<u64>().unwrap()).collect_tuple().unwrap()).collect();
-    println!("task8::part2 - result is {}", part2(junction_boxes, 1000));
+    println!("task8::part2 - result is {}", part2(junction_boxes));
 }
 
 
@@ -155,6 +154,6 @@ mod tests {
             (425,692,689),
         ];
 
-        assert_eq!(part2(junction_boxes, 10), 40);
+        assert_eq!(part2(junction_boxes), 25272);
     }
 }
